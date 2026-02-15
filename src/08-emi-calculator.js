25063f52c8cacff09b688ed07425846aa98290b7
Jadeja Karmadeepsinh <karmadeepjjadeja@gmail.com>
2026-02-15 15:19:30 +0530

    completed question 8

--- src/08-emi-calculator.js
+++ src/08-emi-calculator.js
@@ -43,4 +43,36 @@
  */
 export function calculateEMI(principal, monthlyRate, emi) {
   // Your code here
+  if(typeof principal !== "number" || principal <= 0 || typeof monthlyRate !== "number" || monthlyRate < 0 || typeof emi !== "number" || emi <= 0){
+    return { months: -1, totalPaid: -1, totalInterest: -1 };
+  }
+
+  const firstMonthInterest = principal * monthlyRate;
+  if(emi <= firstMonthInterest){
+    return { months: -1, totalPaid: -1, totalInterest: -1 };
+  }
+
+  let months = 0;
+  let totalPaid = 0;
+  let totalInterest = 0;
+  let remaining = principal;
+
+  while(remaining > 0){
+    let interest = remaining * monthlyRate;
+    totalInterest += interest;
+
+    remaining += interest;
+
+    months++;
+
+    if(remaining <= emi){
+      totalPaid += remaining;
+      remaining = 0;
+    }else{
+      remaining -= emi;
+      totalPaid += emi;
+    }
+  }
+
+  return { months: months, totalPaid: totalPaid, totalInterest: Math.round(totalInterest * 100) / 100 };
 }
